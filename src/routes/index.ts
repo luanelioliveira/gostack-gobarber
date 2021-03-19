c0566bbdde678cf32d053e0607bd5b8e22d8b705
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-03-19 18:34:49 -0300

    validating credentials

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import appoitmentsRouter from "./appointments.routes";
 import usersRouter from "./users.routes";
 import healthCheckRouter from "./healthcheck.routes";
+import sessionRouter from "./session.routes";
 
 const routes = Router();
 
+routes.use("/health", healthCheckRouter);
 routes.use("/appointments", appoitmentsRouter);
 routes.use("/users", usersRouter);
-routes.use("/health", healthCheckRouter);
+routes.use("/sessions", sessionRouter);
 
 export default routes;
