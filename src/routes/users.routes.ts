99a77ff56210b4da2ddec048da48c75b60154dce
Luan Eli Oliveira <luan.oliveira@olxbr.com>
2021-03-27 10:19:25 -0300

    add image uploading

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import uploadConfig from "../config/upload";
 
 const usersRouter = Router();
+const uploader = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -16,10 +20,19 @@ usersRouter.post("/", async (request, response) => {
       password,
     });
 
-    return response.json({user: user.name, email: user.email});
+    return response.json({ user: user.name, email: user.email });
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploader.single("avatar"),
+  async (request, response) => {
+    return response.json({ ok: true });
+  }
+);
 
 export default usersRouter;
